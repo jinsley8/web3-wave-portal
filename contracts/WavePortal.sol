ea19e36dc448740ec71f250f67943cf17a03322a
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2022-02-06 15:23:10 -0500

    feat: New contract for user to wave and get sum of all waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
-  constructor() {
-    console.log("I'm a new smart contract");
-  }
+    uint256 totalWaves;
+
+    constructor() {
+        console.log("I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
 }
